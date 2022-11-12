65260535fa9c7115993a9b71bfdc62b26da7bcaf
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-11-12 13:22:04 -0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# JamesBeardAward
+
+# James Beard Award
+
 a repo for James Beard Award data
---
+++ data/rest&chef.R
@@ -0,0 +1,13 @@
+
+library(tidyverse)
+
+
+winners = tibble(
+  name = c('Puritan Backroom',"El Taco De Mexico"),
+  type = c("America's Classics","America's Classics"),
+  location = c("Manchester, New Hampshire","Denver, Colorado"),
+  category = c("Restaurant & Chef"),
+  year = c(2020, 2020)
+)
+
+winners
